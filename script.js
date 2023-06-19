f00f54e8af2c6256a3b221308f57ba6825d1a6d3
Harri <harri.kinsman@gmail.com>
2023-06-19 19:43:15 +0100

    Add products array

---
+++ script.js
@@ -0,0 +1,20 @@
+// Author: Harri-Nicole Kinsman
+
+// Array of product objects
+
+const products = [
+    {
+      name: "Product 1",
+      image: "product1.jpg",
+      price: "$19.99",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      name: "Product 2",
+      image: "product2.jpg",
+      price: "$24.99",
+      description: "Praesent eu aliquam quam, eget pharetra arcu."
+    },
+    // Add more product objects here
+  ];
+  
