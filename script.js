e044a2663be046cf420c949c30a48ac9284cc007
Harri <harri.kinsman@gmail.com>
2023-06-19 19:45:26 +0100

    Add variables and append products

--- script.js
+++ script.js
@@ -18,3 +18,38 @@ const products = [
     // Add more product objects here
   ];
   
+
+
+  const productList = document.getElementById("product-list");
+  
+  function displayProducts() {
+    products.forEach(product => {
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("product");
+  
+      const productImage = document.createElement("img");
+      productImage.src = product.image;
+  
+      const productName = document.createElement("h2");
+      productName.textContent = product.name;
+  
+      const productPrice = document.createElement("p");
+      productPrice.textContent = `Price: ${product.price}`;
+  
+      const productDescription = document.createElement("p");
+      productDescription.textContent = product.description;
+  
+      const addToCartButton = document.createElement("button");
+      addToCartButton.textContent = "Add to Cart";
+  
+      productDiv.appendChild(productImage);
+      productDiv.appendChild(productName);
+      productDiv.appendChild(productPrice);
+      productDiv.appendChild(productDescription);
+      productDiv.appendChild(addToCartButton);
+  
+      productList.appendChild(productDiv);
+    });
+  }
+  
+  displayProducts();
